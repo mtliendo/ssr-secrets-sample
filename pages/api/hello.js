572942d41828bf5dc510d418608e14b192a725ac
Michael Liendo <mtliendo@gmail.com>
2021-10-20 16:22:46 -0500

    update some stuff

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+	console.log('then secret stuff', process.env.SECRET_TEST)
+	res.status(200).json({ stuff: process.env.SECRET_TEST })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
 
-export default function Home({ secretTest, secretTest2, secretTest3 }) {
-	console.log('the secret test', secretTest)
-	console.log('the secret test2', secretTest2)
-	console.log('the secret test3', secretTest3)
+export default function Home() {
+	useEffect(() => {
+		fetch('/api/hello')
+			.then((res) => res.json())
+			.then((data) => console.log(data))
+	}, [])
 	return <div>Testing</div>
-}
-
-export async function getServerSideProps() {
-	/* trying to see which env vars show up
-  SECRET_TEST is set in the amplify console
-  SECRET_TEST_OTHER is set Amplify console
-  SECRET_TEST_OTHER_ONE is set in parameter store: https://docs.aws.amazon.com/amplify/latest/userguide/environment-variables.html#environment-secrets
-  */
-
-	// Where does this stuff get logged??
-	// Why do the Amplify Console build logs show this as an SSG app and not an SSR app?
-	console.log(process.env.SECRET_TEST)
-	console.log(process.env.secrets?.SECRET_TEST_OTHER)
-	console.log(process.env.secrets?.SECRET_TEST_OTHER_ONE)
-
-	return {
-		props: {
-			secretTest: process.env.SECRET_TEST,
-			secretTest2: process.env.secrets?.SECRET_TEST_OTHER || 'nothing',
-			secretTest3: process.env.secrets?.SECRET_TEST_OTHER_ONE || 'nope',
-		},
-	}
 }
